9f8b00660ea560c1f749dcf17d174fbe76db7a21
Raghav Jindal <jindalraghav98@gmail.com>
2018-11-16 01:41:28 +0530

    District data

---
+++ data/untitled.js
@@ -0,0 +1,356 @@
+var disData = {
+	"district": [
+		{
+
+			"name": "Allahabad",
+			"population": 5954391
+		},
+
+		{
+			"name": "Moradabad",
+			"population": 4772006
+		},
+
+		{
+			"name": "Ghaziabad",
+			"population": 4681645
+		},
+
+		{
+			"name": "Azamgarh",
+			"population": 4613913
+		},
+
+		{
+			"name": "Lucknow",
+			"population": 4589838
+		},
+
+		{
+			"name": "Kanpur Nagar",
+			"population": 4581268
+		},
+
+		{
+			"name": "Jaunpur",
+			"population": 4494204
+		},
+
+		{
+			"name": "Sitapur",
+			"population": 4483992
+		},
+
+		{
+			"name": "Bareilly",
+			"population": 4448359
+		},
+
+		{
+			"name": "Gorakhpur",
+			"population": 4440895
+		},
+
+		{
+			"name": "Agra",
+			"population": 4418797
+		},
+
+		{
+			"name": "Muzaffarnagar",
+			"population": 4143512
+		},
+
+		{
+			"name": "Hardoi",
+			"population": 4092845
+		},
+
+		{
+			"name": "Kheri",
+			"population": 4021243
+		},
+
+		{
+			"name": "Sultanpur",
+			"population": 3797117
+		},
+
+		{
+			"name": "Bijnor",
+			"population": 3682713
+		},
+
+		{
+			"name": "Budaun",
+			"population": 3681896
+		},
+
+		{
+			"name": "Varanasi",
+			"population": 3676841
+		},
+
+		{
+			"name": "Aligarh",
+			"population": 3673889
+		},
+
+		{
+			"name": "Ghazipur",
+			"population": 3620268
+		},
+
+		{
+			"name": "Khushinagar",
+			"population": 3564544
+		},
+
+		{
+			"name": "Bulandshahar",
+			"population": 3499171
+		},
+
+		{
+			"name": "Bahraich",
+			"population": 3487731
+		},
+
+		{
+			"name": "Saharanpur",
+			"population": 3466382
+		},
+
+		{
+			"name": "Meerut",
+			"population": 3443689
+		},
+
+		{
+			"name": "Gonda",
+			"population": 3433919
+		},
+
+		{
+			"name": "Rae Bareli",
+			"population": 3405559
+		},
+
+		{
+			"name": "Barabanki",
+			"population": 3260699
+		},
+
+		{
+			"name": "Ballia",
+			"population": 3239774
+		},
+
+		{
+			"name": "Unnao",
+			"population": 3108946
+		},
+
+		{
+			"name": "Deoria",
+			"population": 3100946
+		},
+
+		{
+			"name": "Shahjahanpur",
+			"population": 3006538
+		},
+
+		{
+			"name": "Maharajganj",
+			"population": 2684703
+		},
+
+		{
+			"name": "Fatehpur",
+			"population": 2632733
+		},
+
+		{
+			"name": "Siddarth Nagar",
+			"population": 2559297
+		},
+
+		{
+			"name": "Mathura",
+			"population": 2547184
+		},
+
+		{
+			"name": "Firozabad",
+			"population": 2498156
+		},
+
+		{
+			"name": "Mirzapur",
+			"population": 2496970
+		},
+
+		{
+			"name": "Faizabad",
+			"population": 2470996
+		},
+
+		{
+			"name": "Basti",
+			"population": 2464464
+		},
+
+		{
+			"name": "Ambedkar Nagar",
+			"population": 2397888
+		},
+
+		{
+			"name": "Rampur",
+			"population": 2335819
+		},
+
+		{
+			"name": "Mau",
+			"population": 2205968
+		},
+
+		{
+			"name": "Balrampur",
+			"population": 2148665
+		},
+
+		{
+			"name": "Pilibhit",
+			"population": 2031007
+		},
+
+		{
+			"name": "Jhansi",
+			"population": 1998603
+		},
+
+		{
+			"name": "Chandauli",
+			"population": 1952756
+		},
+
+		{
+			"name": "Farrukhabad",
+			"population": 1885204
+		},
+
+		{
+			"name": "Mainpuri",
+			"population": 1868529
+		},
+
+		{
+			"name": "Sonbhadra",
+			"population": 1862559
+		},
+
+		{
+			"name": "Jyotiba Phule Nagar",
+			"population": 1840221
+		},
+
+		{
+			"name": "Banda",
+			"population": 1799410
+		},
+
+		{
+			"name": "Rambai Nagar",
+			"population": 1796184
+		},
+
+		{
+			"name": "Etah",
+			"population": 1774480
+		},
+
+		{
+			"name": "Sant Kabir Nagar",
+			"population": 1715183
+		},
+
+		{
+			"name": "Jalaun",
+			"population": 1689974
+		},
+
+		{
+			"name": "Kannauj",
+			"population": 1656616
+		},
+
+		{
+			"name": "Gautam Buddha Nagar",
+			"population": 1648115
+		},
+
+		{
+			"name": "Kaushambi",
+			"population": 1599596
+		},
+
+		{
+			"name": "Etawah",
+			"population": 1581810
+		},
+
+		{
+			"name": "Sant Ravidas Nagar",
+			"population": 1578213
+		},
+
+		{
+			"name": "Mahamaya Nagar",
+			"population": 1564708
+		},
+
+		{
+			"name": "Kanshiram Nagar",
+			"population": 1436719
+		},
+
+		{
+			"name": "Auraiya",
+			"population": 1379545
+		},
+
+		{
+			"name": "Baghpat",
+			"population": 1303048
+		},
+
+		{
+			"name": "Lalitpur",
+			"population": 1221592
+		},
+
+		{
+			"name": "Shrawasti",
+			"population": 1117361
+		},
+
+		{
+			"name": "Hamirpur",
+			"population": 1104285
+		},
+
+		{
+			"name": "Chitrakoot",
+			"population": 991730
+		},
+
+		{
+			"name": "Mahoba",
+			"population": 875958
+		},
+
+
+	]
+}
