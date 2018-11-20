967ba723d1efbee9d019715d330419b23f5c690b
invincibel <hritik.aggarwal31@gmail.com>
2018-11-20 19:14:09 +0530

    addded all necessary links

--- index.php
+++ index.php
@@ -12,6 +12,118 @@
 <script src="data/stateData.js"></script>
 <script src="data/up.geojson"></script>
 <link rel="stylesheet" href="css/index.css"/>
+<style type="text/css">
+	
+	@import url("https://fonts.googleapis.com/css?family=Mukta:700");
+* {
+  box-sizing: border-box;
+}
+*::before, *::after {
+  box-sizing: border-box;
+}
+
+
+body{
+	overflow: hidden;
+}
+button {
+  position: absolute;
+  left: 500px;
+  top: 10%;
+  display: inline-block;
+  cursor: pointer;
+  outline: none;
+  border: 0;
+  vertical-align: middle;
+  text-decoration: none;
+  background: transparent;
+  padding: 0;
+  font-size: inherit;
+  font-family: inherit;
+}
+button.learn-more {
+  width: 12rem;
+  height: auto;
+}
+button.learn-more .circle {
+  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+  position: relative;
+  margin: 0;
+  width: 3rem;
+  height: 3rem;
+  background: #282936;
+  border-radius: 1.625rem;
+}
+button.learn-more .circle .icon {
+  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  background: #fff;
+}
+button.learn-more .circle .icon.arrow {
+  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+  left: 0.625rem;
+  width: 1.125rem;
+  height: 0.125rem;
+  background: none;
+}
+button.learn-more .circle .icon.arrow::before {
+  position: absolute;
+  content: '';
+  top: -0.25rem;
+  right: 0.0625rem;
+  width: 0.625rem;
+  height: 0.625rem;
+  border-top: 0.125rem solid #fff;
+  border-right: 0.125rem solid #fff;
+  -webkit-transform: rotate(45deg);
+          transform: rotate(45deg);
+}
+button.learn-more .button-text {
+  transition: all 0.45s cubic-bezier(0.65, 0, 0.076, 1);
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  padding: 0.75rem 0;
+  margin: 0 0 0 1.85rem;
+  color: #282936;
+  font-weight: 700;
+  line-height: 1.6;
+  text-align: center;
+  text-transform: uppercase;
+}
+button:hover .circle {
+  width: 100%;
+}
+button:hover .circle .icon.arrow {
+  background: #fff;
+  -webkit-transform: translate(1rem, 0);
+          transform: translate(1rem, 0);
+}
+button:hover .button-text {
+  color: #fff;
+}
+
+@supports (display: grid) {
+  body {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 0.625rem;
+    grid-template-areas: ". main main ." ". main main .";
+  }
+
+  #container {
+    grid-area: main;
+    align-self: center;
+    justify-self: center;
+  }
+}
+
+</style>
 </head>
 <body>
 	<div id="map"></div>
@@ -19,6 +131,14 @@
 	<div id="chartContainer" style="height: 33%;position: absolute;right:500px" contentEditable="true" data-text="select a state to see the data"></div>
 	<div id="chartContainr" style="height: 33%;position: absolute;right:500px;top:35%;"></div>
 	<div id="chartContain" style="height: 33%;position: absolute;right:500px;top:65%;"></div>
+</div>
+<div id="container">
+  <a href="http://localhost/Hackethon/home.php"><button class="learn-more">
+    <div class="circle">
+      <span class="icon arrow"></span>
+    </div>
+    <p class="button-text">Report</p>
+  </button></a>
 </div>
 <form action="decide.php" method="get" id="form">
 	<input type="radio" value="" id="state" name="state"><h3 id="header"></h3>
